49e8d834a62443997efd48b4e9411e0e8f24d283
Philip Purwoko <philippurwoko123@gmail.com>
2021-01-23 21:16:53 +0700

    Add drawing app

--- app/main.py
+++ app/main.py
@@ -17,6 +17,10 @@ model = keras.models.load_model('app/ai.h5')
 def home():
     return render_template('home.html', title=title)
 
+@app.route('/drawing', methods=['GET'])
+def drawing():
+    return render_template('drawing.html', title=title)
+
 
 # POST method
 @app.route('/', methods=['POST'])
@@ -40,20 +44,27 @@ def result():
 
 @app.route('/canvas', methods=['POST'])
 def canvas():
+    canvasdata = request.form['canvasimg']
     encoded_data = request.form['canvasimg'].split(',')[1]
+
+    # Decode base64
     nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    cv2.imwrite('28x28.jpg', img)
-    img = img.transpose(2,0,1).reshape(3,-1)
-    img = cv2.resize(img,(28,28))
-    # cv2.imwrite('28x28.jpg', img)
-    img = np.expand_dims(img, axis=0)
+
+    # Convert 3 channel to 1 channel
+    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite('280x280.jpg', gray_image)
+
+    # Resize to (28, 28)
+    gray_image = cv2.resize(gray_image, (28, 28), interpolation=cv2.INTER_LINEAR)
+    cv2.imwrite('28x28.jpg', gray_image)
+
+    # Expand to (1, 28, 28)
+    img = np.expand_dims(gray_image, axis=0)
 
     try:
         prediction = np.argmax(model.predict(img))
-        print(f"Prediction : {str(prediction)}")
-        response = jsonify(response = str(prediction),status = 200)
+        print(f"Prediction Result : {str(prediction)}")
+        return render_template('drawing.html', title=title, response=str(prediction), canvasdata=canvasdata, success=True)
     except Exception as e:
-        response = jsonify(response = str(e),status = 400)
-
-    return response
+        return render_template('drawing.html', title=title, response=str(e), canvasdata=canvasdata)
